1080f976c055d3a082485f0c214feeaad25d9def
Saul385 <Saul385@csu.fullerton.edu>
2022-09-23 15:47:09 -0700

    lexems finding

--- main.cpp
+++ main.cpp
@@ -1,13 +1,37 @@
 #include <iostream>
+#include <fstream>
+#include <string>
 
 using namespace std;
 
-int main ( ) {
-    for ( int i = 22; i <= 100; i = i + 1 ) {
-        while ( i == 100 ) {
-            cout << "Hello World!"; //print out
+void lexer() {
+    //tokens
+    string keywords;
+    string separators;
+    string identifiers;
+    string operators;
+    string real;
+    string punctuation;
+
+    string holder;
+    //Reads txt file
+    ifstream ReadFile;
+    ReadFile.open("input_sourcecode.txt");
+    if(ReadFile.is_open()){
+        //opens first line of txt file
+        while(getline(ReadFile, holder)){
+            //itr through first line
+            for(int i = 0; i < holder.length(); i++) {
+                //checks for specifics in the line (tokens/lexems)
+                if(holder.at(i) == "while") {
+                    keywords = "while";
+                }else if(holder.at(i) == "for") {
+                    keywords = "for";
+                }else if(holder.at(i) == "i") {
+                    identifiers = "i";
+                }
+            }
         }
-    }
-    return 0;
+    }        
 }
 
